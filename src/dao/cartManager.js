aebaf460d50a2cbb9c0183b93ef733d7fe27b5a5
gabriela tururu <gabriela.aguayaire@gmail.com>
2024-03-28 18:05:06 -0600

    cartManager Class - adding persistence via fileSystem (updateProducts in cart function completion pending)

--- src/dao/cartManager.js
+++ src/dao/cartManager.js
@@ -5,28 +5,40 @@ const path= require('path')
 class CartManager{
     static counter=1
 
-    constructor(){
-        this.carts = []
+    constructor(cartFilePath){
+        //this.carts = []
+        this.path=cartFilePath
     }
 
-    getCarts(){
-        return this.carts
+    async getCarts(){
+        // return this.carts
+        if(fs.existsSync(this.path)){
+            return JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+        }else{
+            return []
+        }       
     }
 
-    addProductsToCart(productsArr){
+    async createCartWithProducts(productsArr){
         const cart ={
             cid:'tbd',
             products:productsArr
         }
+        //this.carts.push(cart)     
+        let carts = await this.getCarts()      
+        if(carts.length === 0){
+            cart.cid = 1
+        }else{
+            cart.cid = carts[carts.length-1].cid + 1            
+        }
 
-        cart.cid = CartManager.counter++
-        this.carts.push(cart)
-
-        return `nuevo carrito con id#${cart.cid} fue añadido. ahora hay ${this.carts.length} carrito(s) en total`
+        carts.push(cart)        
+        await fs.promises.writeFile(this.path, JSON.stringify(carts,null,2))
+        return `nuevo carrito con id#${cart.cid} fue añadido. ahora hay ${carts.length} carrito(s) en total`
     }
 
-    getCartById(cid){
-        const existingCarts = this.getCarts()
+    async getCartById(cid){
+        const existingCarts = await this.getCarts()
         const matchingCart = existingCarts.find(cart=>cart.cid===cid)
         if(matchingCart){
             return matchingCart
@@ -35,10 +47,15 @@ class CartManager{
         }
     }
 
-    deleteCart(cid){
-        const allCarts = this.getCarts()
-        const cartToDeleteIndex = allCarts.findIndex(cart=>cart.cid===cid)
+    async deleteCart(cid){
+        let allCarts = await this.getCarts()
+        const cartToDeleteIndex = allCarts.findIndex(cart=>cart.cid===cid)        
+        if(cartToDeleteIndex === -1){
+            console.log('el cart to delete index es-->',cartToDeleteIndex)
+            return `ERROR: El carrito con id#${cid} No fue encontrado (no puede borrarse). Intenta Nuevamente.`
+        }
         allCarts.splice(cartToDeleteIndex,1)       
+        await fs.promises.writeFile(this.path, JSON.stringify(allCarts,null,2))
         return `El carrito con id#${cid} fué borrado exitosamente!.`
     }
 
@@ -57,13 +74,43 @@ class CartManager{
     }
 }
 
-let carrito = new CartManager()
-console.log(carrito.getCarts())
-console.log('agrega : ',carrito.addProductsToCart([{pid:'1',qty:2}, {pid:'4', qty:3}, {pid:'8', qty:1}]))
-console.log('agrega : ',carrito.addProductsToCart([{pid:'10',qty:2}, {pid:'40', qty:3}, {pid:'80', qty:1}]))
-console.log('agrega : ',carrito.addProductsToCart([{pid:'30',qty:2}, {pid:'50', qty:3}, {pid:'90', qty:1}]))
-console.log('carritos al momento POST ADD',carrito.getCarts().map(item=>JSON.stringify(item)))
-console.log('get cart by ID: ',carrito.getCartById(2))
-console.log('carrito product DELETE in cart: ', carrito.deleteProductInCart(1,'4'))
-console.log('DELETE CARRITO: ', carrito.deleteCart(2))
-console.log('carritos remanentes POST CART DELETE',carrito.getCarts().map(item=>JSON.stringify(item)))
+module.exports=CartManager
+
+//testing environment
+// let  cartManagerApp = async()=>{
+//     const cartFilePath = path.join(__dirname, "..", "dao", "carts.json")
+//     let cartManager = new CartManager(cartFilePath)
+//     try{
+//         console.log('FIRST GET CARTS: ',await cartManager.getCarts())
+//         await cartManager.createCartWithProducts([{pid:"1",qty:2},{pid:"4","qty":3},{pid:"8",qty:1}])
+//         console.log(await cartManager.getCarts())
+//         await cartManager.createCartWithProducts([{pid:"10",qty:2},{pid:"40","qty":3},{pid:"80",qty:1}])
+//         await cartManager.createCartWithProducts([{pid:"100",qty:2},{pid:"400","qty":3},{pid:"800",qty:1}])
+//         await cartManager.createCartWithProducts([{pid:"1",qty:2},{pid:"4","qty":3},{pid:"8",qty:1}])
+//         console.log(await cartManager.getCarts())
+//         await cartManager.createCartWithProducts([{pid:"10",qty:2},{pid:"40","qty":3},{pid:"80",qty:1}])
+//         await cartManager.createCartWithProducts([{pid:"100",qty:2},{pid:"400","qty":3},{pid:"800",qty:1}])
+//         let finalCarts = await cartManager.getCarts()
+//         console.log('los final Carts directo-->', finalCarts)
+//         console.log('deleting cart: ',await cartManager.deleteCart(3))
+//         console.log('deleting cart: ',await cartManager.deleteCart(5))
+//         console.log('deleting cart: ',await cartManager.deleteCart(5))
+//         console.log('deleting cart: ',await cartManager.deleteCart(7))
+//     }catch(err){
+//         console.log(err.message)
+//         return
+//     }
+// }
+// cartManagerApp()
+
+//testing
+//let carrito = new CartManager()
+// console.log(carrito.getCarts())
+// console.log('agrega : ',carrito.addProductsToCart([{pid:'1',qty:2}, {pid:'4', qty:3}, {pid:'8', qty:1}]))
+// console.log('agrega : ',carrito.addProductsToCart([{pid:'10',qty:2}, {pid:'40', qty:3}, {pid:'80', qty:1}]))
+// console.log('agrega : ',carrito.addProductsToCart([{pid:'30',qty:2}, {pid:'50', qty:3}, {pid:'90', qty:1}]))
+// console.log('carritos al momento POST ADD',carrito.getCarts().map(item=>JSON.stringify(item)))
+// console.log('get cart by ID: ',carrito.getCartById(2))
+// console.log('carrito product DELETE in cart: ', carrito.deleteProductInCart(1,'4'))
+// console.log('DELETE CARRITO: ', carrito.deleteCart(2))
+// console.log('carritos remanentes POST CART DELETE',carrito.getCarts().map(item=>JSON.stringify(item)))
